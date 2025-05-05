4e5fad9241d66c6277994b20adf10c4b3f89c4d5
norman-brooks <nbprogramming@outlook.com>
2025-05-05 11:07:29 -0700

    Add files via upload

--- ComicBooks/views.py
+++ ComicBooks/views.py
@@ -3,3 +3,5 @@ from django.http import HttpResponse
 # Create your views here.
 def home(request):
     return render(request, 'ComicBooks/home.html')
+def about(request):
+    return render(request, 'ComicBooks/about.html')
